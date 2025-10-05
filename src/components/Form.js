4561f246a7828098a5dfb452027943f5b13098e9
nitaconstantin <104198432+nitaconstantin@users.noreply.github.com>
2025-10-05 15:20:03 +0300

    far away app final

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,27 +1,9 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-export default function App() {
-  return (
-    <div className="app">
-      <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
-  );
-}
-
-function Logo() {
-  return <h1>🌴Far Away 💼</h1>;
-}
-function Form() {
+export default function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  // const [items, setItems] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -35,6 +17,7 @@ function Form() {
     };
 
     console.log(newItem);
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -61,34 +44,5 @@ function Form() {
       />
       <button>Add</button>
     </form>
-  );
-}
-function PackingList() {
-  return (
-    <div className="list">
-      <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-function Item({ item }) {
-  return (
-    <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button>❌</button>
-    </li>
-  );
-}
-function Stats() {
-  return (
-    <footer className="stats">
-      <em> 💼You have X items on your list and you already packed x (X%)</em>
-    </footer>
   );
 }
